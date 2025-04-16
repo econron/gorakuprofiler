5a4c83ecbeb1369756fb750c3addb37b257c8a2e
econron <aroneconbusi5544@gmail.com>
2025-04-16 20:50:36 +0900

    showコマンドを作成

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # gorakuprofiler
-goで楽にプロファイリングするツール
+
+I want a SIMPLE USE tool.
+
+I don't want to think or remenber "HOW TO USE".
+
+I JUST WANT TO PROFILE EASILY.
+
+# HOW TO USE
+
+```go
+package main
+
+import (
+	"log"
+	"fmt"
+	"github.com/econron/gorakuprofiler"
+)
+
+func sum() int32 {
+	var val int32 = 20
+	return val + 200 * 300 / 100
+}
+
+func main() {
+  // This is a example use. JUST COPY AND PASTE THIS.
+	fcpu, err := gorakuprofiler.StartCPU()
+	if err != nil {
+		log.Fatal("failed to init goraku cpu profiler. ", err)
+	}
+	fmem, err := gorakuprofiler.StartMemory()
+	if err != nil {
+		log.Fatal("failed to init goraku memory profiler. ", err)
+	}
+	gorakuprofiler.StopCPU(fcpu)
+	gorakuprofiler.StopMemory(fmem)
+  // This is end.
+
+	ret := sum()
+	fmt.Println(ret)
+}
+```
--- gorakuprofiler.go
+++ gorakuprofiler.go
@@ -1,7 +1,10 @@
 package gorakuprofiler
 
 import (
+	"fmt"
+	"log"
 	"os"
+	"os/exec"
 	"runtime/pprof"
 )
 
@@ -22,10 +25,12 @@ func StopCPU(fCpu *os.File) {
 	defer fCpu.Close()
 	defer pprof.StopCPUProfile()
 }
-
-// show 
-func ShowCPU() {
-	// TODO
+ 
+func ShowCPU(port string) error {
+	if err := startPprofServer("cpu.prof", "8081"); err != nil {
+		return err
+	}
+	return nil
 }
 
 func StartMemory() (*os.File, error) {
@@ -44,6 +49,24 @@ func StopMemory(fMem *os.File) {
 	defer fMem.Close()
 }
 
-func ShowMemory() {
-	// TODO
+func ShowMemory() error {
+	if err := startPprofServer("mem.prof", "8082"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func startPprofServer(path string, port string) error {
+	listenPort := ":" + port
+	cmd := exec.Command("go", "tool", "pprof", fmt.Sprintf("-http=%s", listenPort), path)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+	log.Printf("Started HTTP server on background")
+	log.Printf("If you want to stop server, please stop in your terminal.")
+
+	return nil
 }
